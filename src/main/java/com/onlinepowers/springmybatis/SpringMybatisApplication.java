1b208f3d1c00a926b0b59074d0e8554d612f2c60
keukchang.shin <skc@onlinepowers.com>
2021-02-16 16:21:30 +0900

    [신극창] spring-mybatis 샘플

---
+++ src/main/java/com/onlinepowers/springmybatis/SpringMybatisApplication.java
@@ -0,0 +1,30 @@
+package com.onlinepowers.springmybatis;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import javax.sql.DataSource;
+import org.springframework.core.io.Resource;
+
+@SpringBootApplication
+public class SpringMybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMybatisApplication.class, args);
+	}
+
+	@Bean
+	public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+		SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+		sessionFactory.setDataSource(dataSource);
+
+		Resource[] res = new PathMatchingResourcePatternResolver().getResources("classpath:sqlmapper/**/*-mapper.xml");
+		sessionFactory.setMapperLocations(res);
+
+		return sessionFactory.getObject();
+	}
+}
---
+++ src/test/java/com/onlinepowers/springmybatis/user/UserMapperTest.java
@@ -0,0 +1,23 @@
+package com.onlinepowers.springmybatis.user;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
+@SpringBootTest
+class UserMapperTest {
+
+	@Autowired
+	UserMapper userMapper;
+
+	@Test
+	void getCount() {
+		log.info("## Count: {}", userMapper.getCount());
+		assertThat(userMapper.getCount()).isEqualTo(0);
+	}
+
+}
